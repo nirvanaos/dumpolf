28ed6955a423cb000143ef391ea744dd12c607f5
Igor Popov <popov.nirvana@gmail.com>
2024-04-14 13:44:47 +0300

    Adapt OLF structures for different platforms

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <Nirvana/Nirvana.h>
 #include <Nirvana/OLF_Iterator.h>
+#include <Nirvana/platform.h>
 #include <coffi/coffi.hpp>
 #include <stdint.h>
 #include <iostream>
@@ -45,6 +46,15 @@ public:
       throw std::runtime_error ("File not found");
     if (!reader_.load (file))
       throw std::runtime_error ("Can't process COFF file");
+    machine_ = reader_.get_header ()->get_machine ();
+    bits_ = 32;
+    switch (machine_) {
+    case PLATFORM_X64:
+    case PLATFORM_ARM64:
+      bits_ = 64;
+      break;
+    }
+
     const auto wh = reader_.get_win_header ();
     image_base_ = (uintptr_t)wh->get_image_base ();
     const sections& svec = reader_.get_sections ();
@@ -57,43 +67,69 @@ public:
     }
     if (!olf)
       throw std::runtime_error ("Not a Nirvana module");
-    for (OLF_Iterator it (olf->get_data (), olf->get_data_size ()); !it.end (); it.next ()) {
+
+    if (bits_ == 32)
+      iterate <uint32_t> (*olf);
+    else
+      iterate <uint64_t> (*olf);
+  }
+
+  template <typename Word>
+  void iterate (const section& olf)
+  {
+    for (OLF_Iterator <Word> it (olf.get_data (), olf.get_data_size ()); !it.end (); it.next ()) {
       switch (*it.cur ()) {
-        case OLF_IMPORT_INTERFACE: {
-          auto p = reinterpret_cast <const ImportInterface*> (it.cur ());
-          imports_.push_back ({ get_string (p->name), get_string (p->interface_id) });
-        } break;
+      case OLF_IMPORT_INTERFACE: {
+        auto p = reinterpret_cast <const ImportInterfaceW <Word>*> (it.cur ());
+        imports_.push_back ({ get_string (p->name), get_string (p->interface_id) });
+      } break;
 
-        case OLF_IMPORT_OBJECT: {
-          auto p = reinterpret_cast <const ImportInterface*> (it.cur ());
-          imp_objects_.push_back ({ get_string (p->name), get_string (p->interface_id) });
-        } break;
+      case OLF_IMPORT_OBJECT: {
+        auto p = reinterpret_cast <const ImportInterfaceW <Word>*> (it.cur ());
+        imp_objects_.push_back ({ get_string (p->name), get_string (p->interface_id) });
+      } break;
 
-        case OLF_EXPORT_INTERFACE: {
-          auto p = reinterpret_cast <const ExportInterface*> (it.cur ());
-          Interface::EPV** itf = (Interface::EPV**)translate_addr (p->itf);
-          Interface::EPV* epv = (Interface::EPV*)translate_addr (*itf);
-          exports_.push_back ({ get_string (p->name), get_string (epv->interface_id) });
-        } break;
+      case OLF_EXPORT_INTERFACE: {
+        auto p = reinterpret_cast <const ExportInterfaceW <Word>*> (it.cur ());
+        exports_.push_back ({ get_string (p->name), get_string (get_EPV <Word> (p->itf)->interface_id) });
+      } break;
 
-        case OLF_EXPORT_OBJECT: {
-          auto p = reinterpret_cast <const ExportObject*> (it.cur ());
-          exp_objects_.push_back (get_string (p->name));
-        } break;
+      case OLF_EXPORT_OBJECT: {
+        auto p = reinterpret_cast <const ExportObjectW <Word>*> (it.cur ());
+        exp_objects_.push_back (get_string (p->name));
+      } break;
 
-        case OLF_MODULE_STARTUP: {
-          if (startup_.interface_id)
-            throw std::runtime_error ("Duplicated OLF_MODULE_STARTUP");
-          auto p = reinterpret_cast <const Nirvana::ModuleStartup*> (it.cur ());
-          startup_.interface_id = get_string (get_EPV (p->startup)->interface_id);
-          startup_.flags = p->flags;
-        } break;
+      case OLF_MODULE_STARTUP: {
+        if (startup_.interface_id)
+          throw std::runtime_error ("Duplicated OLF_MODULE_STARTUP");
+        auto p = reinterpret_cast <const Nirvana::ModuleStartupW <Word>*> (it.cur ());
+        startup_.interface_id = get_string (get_EPV <Word> (p->startup)->interface_id);
+        startup_.flags = p->flags;
+      } break;
       }
     }
   }
 
   void print ()
   {
+    const char* platform = "Unknown platform";
+    switch (machine_) {
+    case PLATFORM_I386:
+      platform = "Intel 386";
+      break;
+    case PLATFORM_X64:
+      platform = "AMD64";
+      break;
+    case PLATFORM_ARM:
+      platform = "ARM Little-Endian";
+      break;
+    case PLATFORM_ARM64:
+      platform = "ARM64 Little-Endian";
+      break;
+    }
+
+    std::cout << "Platform: " << platform << std::endl;
+
     if (startup_.interface_id)
       std::cout << "Startup interface: " << startup_.interface_id << std::endl;
     else
@@ -123,17 +159,23 @@ public:
   }
 
 private:
-  const void* translate_addr (const void* p) const;
+  const void* translate_addr (uintptr_t p) const;
 
-  const char* get_string (const char* p)
+  const char* get_string (uintptr_t p)
   {
     return reinterpret_cast <const char*> (translate_addr (p));
   }
 
-  const Interface::EPV* get_EPV (const Interface* itf)
+  template <typename Word>
+  struct InterfaceEPV {
+    Word interface_id;
+  };
+
+  template <typename Word>
+  const InterfaceEPV <Word>* get_EPV (uintptr_t itf)
   {
-    return reinterpret_cast <const Interface::EPV*> (
-      translate_addr (*reinterpret_cast <const void* const*> (translate_addr (itf))));
+    return reinterpret_cast <const InterfaceEPV <Word>*> (
+      translate_addr (*reinterpret_cast <const Word*> (translate_addr (itf))));
   }
 
   static void sort (std::vector <ImpEx>& v);
@@ -142,6 +184,8 @@ private:
 private:
   coffi reader_;
   uintptr_t image_base_;
+  uint16_t machine_;
+  uint16_t bits_;
   Startup startup_;
   std::vector <ImpEx> imports_;
   std::vector <ImpEx> exports_;
@@ -149,9 +193,9 @@ private:
   std::vector <const char*> exp_objects_;
 };
 
-const void* ModuleInfo::translate_addr (const void* p) const
+const void* ModuleInfo::translate_addr (uintptr_t p) const
 {
-  uintptr_t va = (uintptr_t)p - image_base_;
+  uintptr_t va = p - image_base_;
   const section* section = nullptr;
   for (const auto psec : reader_.get_sections ()) {
     uint32_t begin = psec->get_virtual_address ();
